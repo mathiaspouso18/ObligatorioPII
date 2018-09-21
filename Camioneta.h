4eaf2f2e4426f3bceb786e79092a979107054977
Ramiro <ramiro.silva.1993@gmail.com>
2018-09-21 19:39:13 -0300

    Se agrega struct a Camioneta.h. Se agrega referencia a String.h

--- Camioneta.h
+++ Camioneta.h
@@ -1,6 +1,8 @@
 #ifndef CAMIONETA_H_INCLUDED
 #define CAMIONETA_H_INCLUDED
+#include "String.h"
 
-
+typedef struct{String Modelo;
+               long int CapacidadCarga;} Camioneta;
 
 #endif // CAMIONETA_H_INCLUDED
