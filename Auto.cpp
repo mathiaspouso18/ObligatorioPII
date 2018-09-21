ab5b60bcf0902874bd1994d406e93564cd0ad776
Sebastian <sebastiangarciarena@hotmail.com>
2018-09-21 19:52:40 -0300

    Se agregan selectoras y componentes al modulo auto

---
+++ Auto.cpp
@@ -0,0 +1,13 @@
+#include"Auto.h"
+void DarMarca(Auto a, String &s)
+{
+StrCopy(a.Marca,s);
+
+
+}
+
+
+int Daranio(Auto a)
+{
+ return a.anio;
+}
--- Auto.h
+++ Auto.h
@@ -1,6 +1,16 @@
 #ifndef AUTO_H_INCLUDED
 #define AUTO_H_INCLUDED
+#include"String.h"
+typedef struct{
+          String Marca;
+          int anio;
+            }Auto;
 
+
+//Selectora que da marca de Auto
+void DarMarca(Auto a, String &s);
+//Selectora que da anio de fabricacion de auto
+int Daranio(Auto a);
 
 
 #endif // AUTO_H_INCLUDED
